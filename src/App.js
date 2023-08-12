ef15003e754370f2505f9941897e0b027af53680
Miller <windymiller3@yahoo.co.uk>
2023-08-12 19:41:11 +0100

    Static UI Built

--- src/App.js
+++ src/App.js
@@ -9,11 +9,62 @@ const PRODUCTS = [
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
 ];
 
-const ProductRow = ({price, stocked, name}) => {
-  <tr>
-    <td style = {{color: stocked ? 'black' : 'red'}}>{name}</td>
-    <td>{price}</td>
-  </tr>
+const ProductRow = ({ product }) => {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+};
+
+const ProductCategoryRow = ({ category }) => {
+
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+};
+
+const ProductTable = ({ products }) => {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 };
 
 const SearchBar = () => {
@@ -29,10 +80,15 @@ const SearchBar = () => {
   )
 };
 
-export default function FilterableProductTable() {
+ const FilterableProductTable = ({products}) => {
   return (
     <div className="App">
       <SearchBar />
+      <ProductTable products={products}/>
     </div>
   );
 };
+
+export default function App () {
+  return <FilterableProductTable products={PRODUCTS} />
+}
